4ac2f4a65e340317d8f1441c755e71fa86f3c021
juro97 <p.jurassovits@gmail.com>
2022-12-06 08:13:44 +0100

    added base javafx gui and controller skeleton

--- src/main/java/fhtw/ode/messengerapp/ClientController.java
+++ src/main/java/fhtw/ode/messengerapp/ClientController.java
@@ -1,14 +1,21 @@
 package fhtw.ode.messengerapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class ClientController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
 }
